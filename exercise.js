db63da5bd15ac128cfab61bbdfbc7805a3108a2d
Luke <137918569+lnataloNM@users.noreply.github.com>
2023-06-28 18:09:13 +0000

    Update 2 files

--- exercise.js
+++ exercise.js
@@ -1,8 +1,38 @@
 // Your code goes here
+function getItemById(list, id) {
+  return list[id - 1];
+}
+
+function buildTransactions(sales, items) {
+  transaction = [];
+
+  for (i = 0; i < sales.length; i++) {
+    transaction.push({
+      description: items[sales[i].itemId - 1].description,
+      itemId: sales[i].itemId,
+      price: items[sales[i].itemId - 1].price,
+      quantity: sales[i].quantity,
+    });
+  }
+
+  return transaction;
+}
+
+function getTransactionsByItemDescription(transactions, description) {
+  matches = [];
+
+  for (i = 0; i < transactions.length; i++) {
+    if (transactions[i].description === description) {
+      matches.push(transactions[i]);
+    }
+  }
+
+  return matches;
+}
 
 module.exports = {
-    // Uncomment these as you write them
-    // getItemById,
-    // buildTransactions,
-    // getTransactionsByItemDescription
-}
+  // Uncomment these as you write them
+  getItemById,
+  buildTransactions,
+  getTransactionsByItemDescription,
+};
